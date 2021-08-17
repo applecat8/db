473bca32bd347dff31390ce2dd805ada1d21244b
applecat <3075202650@qq.com>
2021-08-17 10:02:20 +0800

    世界上最简单的 SQL 编译器和虚拟机

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # 使用c语言实现一个简单的数据库
 
-[源教程](https://cstack.github.io/db_tutorial/)
+[原教程](https://cstack.github.io/db_tutorial/)
 以Sqlite为模型
+
+## Part 1 - 介绍和设置REPL(交互命令解析器)
+
+[原文part-1](https://cstack.github.io/db_tutorial/parts/part1.html)
+
+## Pare 2 - 世界上最简单的 SQL 编译器和虚拟机
+
+[原文part-2](https://cstack.github.io/db_tutorial/parts/part2.html)
+
+![SQLite 架构](https://cstack.github.io/db_tutorial/assets/images/arch2.gif)
+
--- repl.c
+++ repl.c
@@ -9,10 +9,34 @@ typedef struct {
     ssize_t input_length;
 }InputBuffer;
 
+// 元命令识别结果
+typedef enum{
+    META_SUCCESS,
+    META_UNRECOGNIZED_COMMAND,
+}MetaResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT,
+}PreapareResult;
+
+//语句类型
+typedef enum {
+    INSERT,
+    SELECT,
+}StatementType;
+
+typedef struct{
+    StatementType type;
+} Statement;
+
 InputBuffer* new_input_buffer();
 void print_prompt();
 void read_input(InputBuffer *input_buffer);
 void close_input_buffer(InputBuffer *input_buffer);
+MetaResult do_mate_command(InputBuffer *input_buffer);
+PreapareResult preapare_statement(InputBuffer *input_buffer, Statement *statement);
+void execute_statement(Statement statement);
 
 int main(int agc, char* argv[]){
     InputBuffer *input_buffer = new_input_buffer();
@@ -20,12 +44,27 @@ int main(int agc, char* argv[]){
         print_prompt();
         read_input(input_buffer);
 
-        if (!strcmp(input_buffer->buffer, ".exit")) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        }else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_mate_command(input_buffer)) {
+                case META_SUCCESS:
+                    continue;
+                case META_UNRECOGNIZED_COMMAND:
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
+            }
         }
+
+        Statement statement;
+        switch (preapare_statement(input_buffer, &statement)) {
+            case PREPARE_SUCCESS:
+                break;
+            case PREPARE_UNRECOGNIZED_STATEMENT:
+                printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                continue;
+        }
+
+        execute_statement(statement);
+        printf("Executed.\n");
     }
 }
 
@@ -65,3 +104,39 @@ close_input_buffer(InputBuffer *input_buffer){
     free(input_buffer);
 }
 
+// 识别原名令
+MetaResult
+do_mate_command(InputBuffer *input_buffer){
+    if (!strcmp(input_buffer->buffer, ".exit")) {
+        close_input_buffer(input_buffer);
+        exit(EXIT_SUCCESS);
+    }else {
+        return META_UNRECOGNIZED_COMMAND;
+    }
+}
+
+// 判读语句是否可以执行, 并将可执行的语句类型添加到信息中
+PreapareResult
+preapare_statement(InputBuffer *input_buffer, Statement *statement){
+    if (!strncmp(input_buffer->buffer, "insert", 6)) {
+        statement->type = INSERT;
+        return PREPARE_SUCCESS;
+    }else if (!strcmp(input_buffer->buffer, "select")) {
+        statement->type = SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void
+execute_statement(Statement statement){
+    switch (statement.type) {
+        case INSERT:
+            printf("This is a inset statement\n");
+            break;
+        case SELECT:
+            printf("This is a select statement\n");
+            break;
+    }
+}
